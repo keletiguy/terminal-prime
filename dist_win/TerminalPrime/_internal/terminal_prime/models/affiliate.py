b07227ddb191381927da2ff5c132503e8f862bea
Terminal Prime Dev <dev@terminalprime.local>
2026-04-02 14:38:30 +0000

    chore: remove build artifacts from repo

--- dist_win/TerminalPrime/_internal/terminal_prime/models/affiliate.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-
-
-@dataclass
-class Affiliate:
-    name: str
-    client_id: int
-    id: Optional[int] = None
--- dist_win/TerminalPrime/_internal/terminal_prime/models/client.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-
-
-@dataclass
-class Client:
-    name: str
-    id: Optional[int] = None
-    contact_email: Optional[str] = None
